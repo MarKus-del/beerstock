0de33e97769da703e637a4bfbb1c4ad872a74bde
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-05 21:44:13 -0300

    criado projeto atraves do initialzn

---
+++ src/test/java/com/markusdel/beerstock/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markusdel.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
