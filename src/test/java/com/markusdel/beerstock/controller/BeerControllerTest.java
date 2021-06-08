0edbd2af7f5448f3f02ff31409fe2c1e458b1a45
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-08 13:47:44 -0300

    testes para validar os tipos de retornos do endpoint de exclução de cervejas

--- src/test/java/com/markusdel/beerstock/controller/BeerControllerTest.java
+++ src/test/java/com/markusdel/beerstock/controller/BeerControllerTest.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 
 import static com.markusdel.beerstock.utils.JsonConvertionUtils.asJsonString;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -140,5 +140,30 @@ public class BeerControllerTest {
         mockMvc.perform(get(BEER_API_URL_PATH)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenDELETEItsCalledWithValidIdThenNoContentStatusIsReturned() throws Exception {
+        // given
+        BeerDTO beerDTO = BeerDTPBuilder.builder().build().toBeerDTO();
+
+        // when
+        doNothing().when(beerService).deleteById(beerDTO.getId());
+
+        // then
+        mockMvc.perform(delete(BEER_API_URL_PATH + "/" + beerDTO.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void whenDELETEItsCalledWithInvalidIdThenNoFoundStatusIsReturned() throws Exception {
+        // when
+        doThrow(BeerNotFoundException.class).when(beerService).deleteById(INVALID_BEER_ID);
+
+        // then
+        mockMvc.perform(delete(BEER_API_URL_PATH + "/" + INVALID_BEER_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
